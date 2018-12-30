2b3de1a93191640e6c9e3ec7f372a075b20aa7de
capedcrusader23 <37747717+capedcrusader23@users.noreply.github.com>
2018-12-30 10:55:22 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -3,14 +3,26 @@ const ejs=require('ejs')
 const path=require('path')
 const route=require('./route/route.js')
 const app=express();
+const pass=require('passport')
 const mongoose=require('mongoose')
+const config=require('./config/local.js')
+const flash=require('express-flash')
+const session=require('express-session')
 mongoose.connect('mongodb://uphaar:uphaar23@ds145434.mlab.com:45434/converge',{ useNewUrlParser: true }).then(()=>{
   console.log("DB CONNECTED")
 })
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
+app.use(pass.initialize());
+app.use(pass.session());
 app.set('view engine','ejs');
-app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({ cookie: { maxAge: 60000 },
+                  secret: 'woot',
+                  resave: false,
+                  saveUninitialized: false}));
+app.use(flash())
 app.use(route);
 
 
